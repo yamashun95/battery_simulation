608432541e43d80b2adca26ae8cc1aecf7baf0ab
yamashun99 <yamashun.103099@gmail.com>
2024-06-15 21:27:35 +0900

    境界条件実装したが解が発散する

--- src/spm/electrode_dynamics.py
+++ src/spm/electrode_dynamics.py
@@ -3,55 +3,54 @@ import matplotlib.pyplot as plt
 from utils import *
 
 
-def simulate_electrochemical_system(r0, nu, a, U0, F_RT, D, Nx, Ntau, t_max):
-    x = np.linspace(0, 1, Nx)
-    t = np.linspace(0, t_max, Ntau)
+def simulate_electrochemical_system(x, t, Uapp, r0, a, F_RT, D):
+    Nx = len(x)
+    Ntau = len(t)
     tau = t * D / r0**2
     dx = x[1] - x[0]
     dtau = tau[1] - tau[0]
-    alpha0 = dtau / dx**2
-    alpha1 = 2 * dtau / dx
-    Uapp = U0 + nu * t
-    y = np.ones(Nx) * 0.99
+    alpha0 = dtau / (2 * dx**2)
+    alpha1 = dtau / (2 * dx)
+    y = np.ones(Nx) * 0.992
     js = np.zeros(Ntau)
+    A = np.zeros((Nx, Nx))
+    B = np.zeros((Nx, Nx))
+    for i in range(1, Nx - 1):
+        A[i, i - 1] = -alpha0 + alpha1 / x[i]
+        A[i, i] = 1 + 2 * alpha0
+        A[i, i + 1] = -alpha0 - alpha1 / x[i]
+        B[i, i - 1] = alpha0 - alpha1 / x[i]
+        B[i, i] = 1 - 2 * alpha0
+        B[i, i + 1] = alpha0 + alpha1 / x[i]
+    g0 = 0
+    A[0, 0] = 1 + 2 * alpha0
+    A[0, 1] = -2 * alpha0
+    A[-1, -2] = -2 * alpha0
+    A[-1, -1] = 1 + 2 * alpha0
+    b = np.dot(B, y)
+    print(b)
+    b[0] = (1 - 2 * alpha0) * y[0] + 2 * alpha0 * y[1]
 
     for itau in range(Ntau):
-        yn = np.zeros(Nx)
-        print(f"ddx:{alpha0*(y[Nx - 1] - 2 * y[Nx - 2] + y[Nx - 3])}")
-        for i in range(1, Nx - 1):
-            yn[i] = (
-                y[i]
-                + alpha0 * (y[i + 1] - 2 * y[i] + y[i - 1])
-                # + alpha1 / x[i] * (y[i + 1] - y[i])
-            )
-        yn[0] = yn[1]
-        print(
-            -dx
-            * a
-            * np.sqrt((1.0 - yn[-2]) * (yn[-2]))
+        gN = (
+            -a
+            * np.sqrt((1.0 - y[-1]) * (y[-1]))
             * 2
-            * np.sinh(0.5 * F_RT * (Uapp[itau] - ocv_LMO(yn[-2])))
+            * np.sinh(0.5 * F_RT * (Uapp[itau] - ocv_LMO(y[-1])))
         )
-        yn[-1] = yn[-2] - dx * a * np.sqrt((1.0 - yn[-2]) * (yn[-2])) * 2 * np.sinh(
-            0.5 * F_RT * (Uapp[itau] - ocv_LMO(yn[-2]))
+        b[-1] = (
+            2 * alpha0 * y[-2]
+            + (1 - 2 * alpha0) * y[-1]
+            + 4 * gN * dx * (alpha0 + alpha1 / x[-1])
         )
-        print(yn[-10:])
+        yn = np.linalg.solve(A, b)
         y = yn
+        print(y[-10:])
         js[itau] = (
             a
             * np.sqrt((1.0 - y[-1]) * (y[-1]))
             * 2
             * np.sinh(0.5 * F_RT * (Uapp[itau] - ocv_LMO(y[-1])))
         )
-        print(y[-1])
 
-    return y, js, tau
-
-
-def plot_results(tau, js):
-    plt.figure()
-    plt.plot(tau, js)
-    plt.xlabel("Tau")
-    plt.ylabel("Current Density (j)")
-    plt.title("Current Density vs. Time")
-    plt.show()
+    return y, js, tau, Uapp
